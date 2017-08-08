ac284402c760cd1c7ec8c4054c958fd42d605dfa
Alexander Steele <alexsteele110@gmail.com>
2017-08-08 15:51:26 -0400

    added callback route

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get(
   })
 );
 
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server is running...');
