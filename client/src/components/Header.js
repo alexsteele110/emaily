9373bf09aa91734308b40f434c2de1f119b8a85a
Alexander Steele <alexsteele110@gmail.com>
2017-08-13 15:59:12 -0400

    auth responsiveness to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Google Sign In</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="#">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="brand-logo center">Emaily</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +35,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
