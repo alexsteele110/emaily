7cfdbbe3718a8f06797dd14e3413dc28920dcd0e
Alexander Steele <alexsteele110@gmail.com>
2017-08-15 12:03:33 -0400

    stipe init setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -25,7 +26,9 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="brand-logo center">Emaily</a>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
+            Emaily
+          </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             {this.renderContent()}
           </ul>
