67a78ba5a596bf02791b11a06098e33c239f74e3
Alexander Steele <alexsteele110@gmail.com>
2017-08-15 15:49:24 -0400

    billing route done, require login middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,19 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    console.log(req.body);
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: 'Sample charge',
+      source: req.body.id
+    });
+
+    req.user.credits += 5; //modifies user data
+    const user = await req.user.save(); //saves the changes we made to database, asynchronous
+
+    res.send(user); //sends back updated user info to us
   });
 };
