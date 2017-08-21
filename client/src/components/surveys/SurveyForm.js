1df2f61ba801f7dac4e1b1d753f585b8b0687046
Alexander Steele <alexsteele110@gmail.com>
2017-08-21 13:57:20 -0400

    basic survey form setup

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,56 @@
+// SurveyForm shows a form for a user to add input
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link
+            to="/surveys"
+            className="red lighten-2 btn waves-effect waves-light"
+          >
+            Cancel
+          </Link>
+          <button
+            className="btn waves-effect waves-light right"
+            type="submit"
+            name="action"
+          >
+            Next<i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
